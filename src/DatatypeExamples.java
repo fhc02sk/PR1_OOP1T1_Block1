828898a6f3af415d84b56b86cab5ec3ccd14ac61
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2021-03-04 13:33:23 +0100

    DatatypeExamples

--- src/DatatypeExamples.java
+++ src/DatatypeExamples.java
@@ -45,5 +45,13 @@ public class DatatypeExamples {
         * a / b ...
         * a % b ...
         *  */
+        System.out.println("a + b = " + a + " + " + b + " = " );
+
+        a = 11;
+        b = 3;
+        System.out.println("a + b = " + a + " + " + b + " = " );
+
+        // float x, y, result2; => kein modulo %
     }
+
 }
